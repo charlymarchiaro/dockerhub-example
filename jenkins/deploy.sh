042c8e573162d97b646169ca19dd868d852ca329
Charly Marchiaro <carlosmarchiaro@gmail.com>
2021-06-03 23:19:46 -0300

    Update deploy.sh

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -8,4 +8,11 @@ kubectl \
   --server=$KUBERNETES_SERVER \
   --certificate-authority=cert.crt \
   --token=$KUBERNETES_TOKEN \
-  set image deployments/dp-alpine-deployment dp-alpine=charlymarchiaro/dp-alpine:$SHA
+  delete deployment dp-alpine-deployment
+
+kubectl \
+  --kubeconfig=/dev/null \
+  --server=$KUBERNETES_SERVER \
+  --certificate-authority=cert.crt \
+  --token=$KUBERNETES_TOKEN \
+  apply -f k8s/deployments/dp-alpine-deployment.yaml
